9b09a3edd9f003023e474a2848c718dae4a79df1
yaozhikun <yaozhikun.whlpkk1@bytedance.com>
2023-01-29 15:37:17 +0800

    修改

--- Swift/Sort/InsertSort.swift
+++ Swift/Sort/InsertSort.swift
@@ -10,12 +10,15 @@ import Foundation
 
 /*
  插入排序
- 
  1、在已经有序的小序列的基础上，一次插入一个元素；
- 2、想要插入的元素和已经有序的最大者开始比起，如果比它大则直接插入在其后面，否则一直往前找直到找到它该插入的位置；
+ 2、想要插入的元素和已经有序的最大者开始比起，如果比它大则直接插入在其后面，否则一直往前找直到找到它该插入的位置；（由于数组是连续的数据结构，所以插入操作需要移动很多元素，这里使用冒泡）
  3、如果碰见一个和插入元素相 等的，那么插入元素把想插入的元素放在相等元素的后面；
  4、相等元素的前后顺序没有改变；
  5、稳定排序算法。
+ 
+ 时间复杂度：O(n)~O(n²)，平均时间复杂度为O(n²)。
+ 在完全有序的情况下，插入排序每个未排序区间元素只需要比较1次，所以时间复杂度是O(n)。
+ 而在极端情况完全逆序，时间复杂度为O(n²).
  */
 
 class InsertSort <Element: Comparable> {
@@ -26,7 +29,8 @@ class InsertSort <Element: Comparable> {
 
     public static func sort(_ array: inout [Element], by compareOrderBy:(Element, Element) -> Bool) {
         for i in 1..<array.count {
-            for j in (1...(i)).reversed() {
+            //将a[i]插入到a[i-1]，a[i-2] ... a[0]之中
+            for j in (1...(i)).reversed() {  //由于a[0],a[1]...a[i-1]已经有序，所以j从i开始冒泡，即可调整好i的位置
                 if compareOrderBy(array[j], array[j-1]) {
                     swap(&array, j-1, j)
                 } else {
--- Swift/Sort/MergeSort.swift
+++ Swift/Sort/MergeSort.swift
@@ -10,10 +10,11 @@ import Foundation
 
 /*
  归并排序
-
  1、把序列递归地分成短序列，递归出口是短序列只有1个元素(认为直接有序),然后把各个有序的短序列合并成一个有序的长序列，不断合并直到原序列全部排好序；
  2、合并过程中我们可以保证如果两个当前元素相等时，我们把处在前面的序列的元素保存在结 果序列的前面，这样就保证了稳定性；
  3、稳定排序算法。
+ 
+ 时间复杂度：O(n㏒n)，每次2分，总计划分㏒𝑛层子区间，每层复杂度为Θ(𝑛)。
  */
 
 //2路归并
@@ -40,7 +41,7 @@ class MergeSort<Element: Comparable> {
         guard low < high else {
             return
         }
-        let middle = (high - low) / 2 + low
+        let middle = (high + low) / 2
         mergeSortAction(&array, tmp: &tmp, low: low, high: middle, by: compareOrderBy)
         mergeSortAction(&array, tmp: &tmp, low: middle + 1, high: high, by: compareOrderBy)
         merge(&array, tmp: &tmp, low: low, middle: middle, high: high, by: compareOrderBy)
@@ -48,9 +49,9 @@ class MergeSort<Element: Comparable> {
     
     // 循环版
     static func mergeSortAction2(_ array: inout [Element], tmp: inout [Element?], low: Int, high: Int, by compareOrderBy:(Element, Element) -> Bool) {
-        var size = 1
+        var size = 1 //当前要归并数组的长度
         var l=0, mid=0, r=0
-        while size <= high {
+        while size <= high {  //循环依次归并数组前size*2个元素，即前2,4,8,16 ... 个元素
             l = 0
             while l+size <= high {
                 mid = l+size-1
@@ -71,16 +72,16 @@ class MergeSort<Element: Comparable> {
         }
         var l = low
         var r = middle + 1
-        var i = low
+        var pos = low
         while l <= middle || r <= high {
-            if r > high || (l<=middle && !compareOrderBy(array[r], array[l])) {
-                tmp[i] = array[l]
+            if r > high || (l<=middle && compareOrderBy(array[l], array[r])) {
+                tmp[pos] = array[l]
                 l += 1
             } else {
-                tmp[i] = array[r]
+                tmp[pos] = array[r]
                 r += 1
             }
-            i += 1
+            pos += 1
         }
         for i in low...high {
             array[i] = tmp[i]!
